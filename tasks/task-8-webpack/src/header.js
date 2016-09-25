a80448e27cb359135076a095530915486d557cc1
Kris-Mikael Krister <krismikael@protonmail.com>
2016-09-25 17:41:50 +0200

    Add propTypes to header.js

--- tasks/task-8-webpack/src/header.js
+++ tasks/task-8-webpack/src/header.js
@@ -1,9 +1,13 @@
-import React, {Component} from 'react';
+import React, {Component, PropTypes} from 'react';
 
 class Header extends Component {
     render() {
         return <p>Hei {this.props.yourName}!</p>
     }
 }
+
+Header.propTypes = {
+    yourName: PropTypes.string.isRequired
+};
 
 export default Header;
