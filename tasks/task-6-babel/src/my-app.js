2b1504cb488fc7670e3e93e8f5324b53af12fb47
Kris-Mikael Krister <krismikael@protonmail.com>
2016-09-25 11:03:53 +0200

    Revert "Include task 6 content written during workshop"
    This reverts commit 892baab780dc060512752728db59d5b95e403be9.

--- tasks/task-6-babel/src/my-app.js
+++ tasks/task-6-babel/src/my-app.js
@@ -1,59 +1,7 @@
-class Header extends React.Component {
+class MyClass extends React.Component {
     render() {
-        return <h1>Hei {this.props.yourName}</h1>
+        return <div>Inne i min klasse</div>
     }
 }
 
-
-class MovieList extends React.Component {
-    constructor() {
-        super();
-        this.state = {
-            inputValue: '',
-            myFavoriteMovies: [
-                'Blade Runner',
-                'Kung Fury',
-                'Song of the Sea'
-            ]
-        };
-    }
-    handleChange(event) {
-        this.setState({
-            inputValue: event.target.value
-        });
-    }
-    handleClick() {
-        const newMovieName = this.state.inputValue;
-        this.setState({
-            myFavoriteMovies: [
-                ...this.state.myFavoriteMovies,
-                newMovieName
-            ]
-        });
-    }
-    render() {
-        return (
-            <div>
-                <ul>
-                    {this.state.myFavoriteMovies.map(function (favoriteMovie) {
-                        return (
-                            <li key={favoriteMovie}>
-                                {favoriteMovie}
-                            </li>
-                        );
-                    })}
-                </ul>
-                <input onChange={this.handleChange.bind(this)}/>
-                <button onClick={this.handleClick.bind(this)}>Legg til film</button>
-            </div>
-        );
-    }
-}
-
-ReactDOM.render(
-    <div>
-        <Header yourName="Kris-Mikael" />
-        <MovieList/>
-    </div>,
-    document.getElementById('my-app')
-);
+ReactDOM.render(<MyClass/>, document.getElementById('my-app'));
